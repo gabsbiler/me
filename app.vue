b332aedc07f6f7969d93fbbe4a065a9e08600506
Gabriel Biler <gabsbiler@gmail.com>
2024-02-02 16:48:23 +0800

    Init Maintenance Mode

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div id="main-body">
+    <!-- <NuxtWelcome /> -->
+    <h1>Maintenance Mode</h1>
   </div>
 </template>
+
+<style>
+#main-body {
+  display: flex;
+  height: 100vh;
+}
+
+h1{
+  margin-inline: auto;
+  margin-block:auto;
+}
+</style>
